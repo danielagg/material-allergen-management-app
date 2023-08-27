45d2b9c50a774d7a95255ddee7b9fee7c202d77e
Daniel Agg <daniel.agg@outlook.com>
2023-08-27 13:08:27 +0200

    upgrade skeleton, wintry theme

--- MaterialAllergenManagementApp.Client/src/routes/+layout.svelte
+++ MaterialAllergenManagementApp.Client/src/routes/+layout.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-	import '@skeletonlabs/skeleton/themes/theme-modern.css';
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppShell, Drawer, initializeStores } from '@skeletonlabs/skeleton';
+	import TopBar from '$lib/components/TopBar.svelte';
+	import SideBar from '$lib/components/SideBar.svelte';
 
-	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	import TopBar from '../lib/components/TopBar.svelte';
-	import SideBar from '../lib/components/SideBar.svelte';
 
-	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow }); // popup
+	initializeStores(); // drawers
 </script>
 
-<AppShell>
+<Drawer />
+<AppShell class="min-h-screen">
 	<svelte:fragment slot="header">
 		<TopBar />
 	</svelte:fragment>
@@ -20,6 +21,8 @@
 	<svelte:fragment slot="sidebarLeft">
 		<SideBar />
 	</svelte:fragment>
+
+	<Drawer />
 
 	<slot />
 </AppShell>
--- MaterialAllergenManagementApp.Client/src/routes/+page.svelte
+++ MaterialAllergenManagementApp.Client/src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
 	import { Table } from '@skeletonlabs/skeleton';
-	import type { TableSource } from '@skeletonlabs/skeleton';
+	import type { DrawerSettings, TableSource } from '@skeletonlabs/skeleton';
 	import { tableMapperValues } from '@skeletonlabs/skeleton';
+	import { Paginator } from '@skeletonlabs/skeleton';
+	// import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+	const drawerSettings: DrawerSettings = {
+		id: 'allergen-filters',
+		// bgBackdrop: 'bg-gradient-to-tr from-indigo-500/50 via-purple-500/50 to-pink-500/50',
+		bgBackdrop: 'bg-gradient-to-tr from-primary-500/50 via-tertiary-500/50 to-secondary-500/50',
+
+		width: 'w-[600px]',
+		padding: 'p-4',
+		rounded: 'rounded-xl',
+		position: 'right'
+	};
 
 	const sourceData = [
 		{
@@ -9,64 +25,50 @@
 			id: 'R10000000562943',
 			type: 'Food',
 			name: 'Hydrogen',
-			weight: 1.0079,
-			symbol: 'H'
+			createdOn: '2023-08-16T13:23:50.0649867'
 		},
 		{
 			position: 2,
 			id: 'P10000001279912',
 			type: 'Perishable',
 			name: 'Helium',
-			weight: 4.0026,
-			symbol: 'He'
+			createdOn: '2023-07-18T13:23:50.0649867'
 		},
 		{
 			position: 3,
 			id: 'R10000000499901',
 			type: 'Beverage',
-			name: 'Lithium',
-			weight: 6.941,
-			symbol: 'Li'
+			name: 'Lithium'
 		},
 		{
 			position: 4,
 			id: 'R10000000499899',
 			type: 'Finished Product',
-			name: 'Beryllium',
-			weight: 9.0122,
-			symbol: 'Be'
+			name: 'Beryllium'
 		},
 		{
 			position: 5,
 			id: 'P10000001198112',
 			type: 'Simple Packaging Material',
-			name: 'Boron',
-			weight: 10.811,
-			symbol: 'B'
+			name: 'Boron'
 		},
 		{
 			position: 6,
 			id: 'P10000001135292',
 			type: 'Simple Packaging Material',
-			name: 'Boron',
-			weight: 10.811,
-			symbol: 'B'
+			name: 'Boron'
 		},
 		{
 			position: 7,
 			id: 'P10000001098812',
 			type: 'Manufacturer Part',
-			name: 'Boron',
-			weight: 10.811,
-			symbol: 'B'
+			name: 'Boron'
 		},
 		{
 			position: 7,
 			id: 'R10000000312899',
 			type: 'Finished Product',
-			name: 'Boron',
-			weight: 10.811,
-			symbol: 'B'
+			name: 'Boron'
 		}
 	];
 
@@ -88,10 +90,31 @@
 		<p class="w-2/3 mt-2 text-sm opacity-60">
 			Use this page to manage the allergen classification of each material.
 		</p>
+		<div class="mt-6 flex justify-between items-center space-x-2">
+			<input class="input" type="text" placeholder="Search for materials..." />
+			<button
+				type="button"
+				class="btn variant-filled"
+				on:click={() => drawerStore.open(drawerSettings)}
+			>
+				<span>
+					<Icon icon="ion:filter" style="font-size: 24px" />
+				</span>
+				<span>Filters</span>
+			</button>
+		</div>
 		<Table
 			source={tableSimple}
 			interactive={true}
-			class="mt-6 border border-surface-500 shadow-xl "
+			class="mt-4 border border-surface-500 shadow-xl "
 		/>
+		<div class="mt-4">
+			<Paginator
+				showPreviousNextButtons
+				showFirstLastButtons
+				maxNumerals={1}
+				settings={{ page: 0, limit: 10, size: 50, amounts: [5, 10, 20] }}
+			/>
+		</div>
 	</div>
 </div>
