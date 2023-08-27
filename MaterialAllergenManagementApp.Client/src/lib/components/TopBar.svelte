fb2006aefd1e58c315605bbc3e2f44c6bacbf8da
Daniel Agg <daniel.agg@outlook.com>
2023-08-27 21:45:55 +0200

    remove avatar component

--- MaterialAllergenManagementApp.Client/src/lib/components/TopBar.svelte
+++ MaterialAllergenManagementApp.Client/src/lib/components/TopBar.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-	import { AppBar, type PopupSettings } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
-	import { Avatar } from '@skeletonlabs/skeleton';
-	import { popup } from '@skeletonlabs/skeleton';
 	import Icon from '@iconify/svelte';
-
-	const popupFeatured: PopupSettings = {
-		event: 'click',
-		target: 'popupFeatured',
-		placement: 'bottom'
-	};
 </script>
 
 <AppBar
@@ -27,18 +19,5 @@
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<LightSwitch />
-		<div use:popup={popupFeatured}>
-			<Avatar
-				border="border-4 border-surface-300-600-token hover:!border-primary-500"
-				cursor="cursor-pointer"
-				width="w-12"
-				initials="AD"
-			/>
-		</div>
-
-		<div class="card p-4 w-72 shadow-xl" data-popup="popupFeatured">
-			<div><p>Demo Content</p></div>
-			<div class="arrow bg-surface-100-800-token" />
-		</div>
 	</svelte:fragment>
 </AppBar>
