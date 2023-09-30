8ac0ca214ce3c7eaeda367a642970b122e5dd278
Daniel Agg <daniel.agg@outlook.com>
2023-09-30 17:29:43 +0200

    IaC for app service plan, app service + Dockerfile

--- MAM.IaC/main.tf
+++ MAM.IaC/main.tf
@@ -1,4 +1,32 @@
 resource "azurerm_resource_group" "main-resource-group" {
   name     = var.resource_group_name
-  location = "West EU"
+  location = "westeurope"
+}
+
+resource "azurerm_service_plan" "mam-service-plan" {
+  name                = "mam-service-plan"
+  location            = azurerm_resource_group.main-resource-group.location
+  resource_group_name = azurerm_resource_group.main-resource-group.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "mam-api" {
+  name                = "mam-api"
+  location            = azurerm_resource_group.main-resource-group.location
+  resource_group_name = azurerm_resource_group.main-resource-group.name
+  service_plan_id     = azurerm_service_plan.mam-service-plan.id
+  https_only          = true
+  
+  site_config {
+    always_on = false
+
+    application_stack {
+      docker_image_name = "mcr.microsoft.com/dotnet/aspnet:8.0-alpine"
+    }
+
+    cors {
+      allowed_origins = ["http://localhost:5173/", "https://mam.danielagg.com/"]
+    }
+  }
 }
