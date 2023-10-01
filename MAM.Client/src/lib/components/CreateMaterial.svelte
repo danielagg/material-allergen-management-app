e9f4c1f2e50832e44d04d4780819f492249764e4
Daniel Agg <daniel.agg@outlook.com>
2023-10-01 22:26:46 +0200

    modular styling attempt

--- MAM.Client/src/lib/components/CreateMaterial.svelte
+++ MAM.Client/src/lib/components/CreateMaterial.svelte
@@ -5,8 +5,6 @@
 	import CurrentStockStep from './create-new-material-steps/CurrentStockStep.svelte';
 	import { superForm } from 'sveltekit-superforms/client';
 	import SummaryStep from './create-new-material-steps/SummaryStep.svelte';
-	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
-	import { dev } from '$app/environment';
 
 	// todo: form should be coming from the +page.svelte file under the (protected) root
 	const { form, enhance } = superForm(
@@ -31,8 +29,8 @@
 		!$form.code || !$form.shortName || !$form.fullName || !$form.materialTypeId;
 </script>
 
-<form action="?/createMaterial" method="POST" use:enhance>
-	<div class="card p-8 w-modal shadow-xl space-y-4 min-w-[50vw]">
+<div class="card p-8 w-modal shadow-xl space-y-4 min-w-[50vw]">
+	<form action="?/createMaterial" method="POST" use:enhance>
 		<Stepper buttonCompleteLabel="Create Material" buttonCompleteType="submit">
 			<Step locked={isBasicStepIncomplete}>
 				<svelte:fragment slot="header">Create New Material: Basic details</svelte:fragment>
@@ -64,6 +62,5 @@
 				<SummaryStep {form} />
 			</Step>
 		</Stepper>
-		<SuperDebug data={$form} display={dev} />
-	</div>
-</form>
+	</form>
+</div>
